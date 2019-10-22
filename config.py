5b77a083a3f6b01010e9c7bf8e0e44553cdb56bf
Shen-GuoXin <34859237+Shen-GuoXin@users.noreply.github.com>
2019-10-22 09:35:49 +0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from py2neo import Graph
-import os
-project_dir = os.path.dirname(os.path.abspath(__file__))
-
-graph = Graph(
-    "http://localhost:7474",
-    username="neo4j",
-    password="sgxsgwbd"
-)
